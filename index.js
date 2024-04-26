54e3eeb3301d88443a57e0464139fa94265203ce
TanveerAhmed4545 <tanveer8507@gmail.com>
2024-04-27 02:12:54 +0600

    add get craftitem edit and add for put

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/craftItem/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await craftCollection.findOne(query);
+        res.send(result);
+    })
+
     // My crafts
     app.get('/craftItems/:userEmail',async(req,res)=>{
         const result = await craftCollection.find({userEmail:req.params.userEmail}).toArray();
